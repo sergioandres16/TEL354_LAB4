6a5afad96f9435795dfd752407d99fcfe9259baf
Sergio <sergio.sotomayor@pucp.edu.pe>
2023-10-28 23:23:07 -0500

    comit

--- src/main/java/net/floodlightcontroller/mactracker/MACTracker.java
+++ src/main/java/net/floodlightcontroller/mactracker/MACTracker.java
@@ -28,67 +28,76 @@ import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
 
-public class MACTracker implements IOFMessageListener, IFloodlightModule {
+public class MACTracker implements IOFMessageListener, IFloodlightModule, IMACTrackerService {
     protected IFloodlightProviderService floodlightProvider;
     protected Set<Long> macAddresses;
     protected static Logger logger;
+    protected IRestApiService restApi;
+
     @Override
     public String getName() {
+// TODO Auto-generated method stub
         return MACTracker.class.getSimpleName();
     }
-
     @Override
     public boolean isCallbackOrderingPrereq(OFType type, String name) {
-        // TODO Auto-generated method stub
-        return false;
+// TODO Auto-generated method stub
+        return (type.equals(OFType.PACKET_IN) && name.equals("forwarding"));
     }
-
     @Override
     public boolean isCallbackOrderingPostreq(OFType type, String name) {
-        // TODO Auto-generated method stub
+// TODO Auto-generated method stub
         return false;
     }
-
     @Override
     public Collection<Class<? extends IFloodlightService>> getModuleServices() {
-        // TODO Auto-generated method stub
-        return null;
+// TODO Auto-generated method stub
+        Collection<Class<? extends IFloodlightService>> array = new ArrayList<Class<? extends
+                IFloodlightService>>();
+        array.add(IMACTrackerService.class);
+        return array;
     }
-
     @Override
     public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
-        // TODO Auto-generated method stub
-        return null;
+// TODO Auto-generated method stub
+        Map<Class<? extends IFloodlightService>, IFloodlightService> hm = new HashMap<Class<?
+                extends IFloodlightService>, IFloodlightService>();
+        hm.put(IMACTrackerService.class, this);
+        return hm;
     }
-
     @Override
     public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
-        Collection<Class<? extends IFloodlightService>> l =
-                new ArrayList<Class<? extends IFloodlightService>>();
-        l.add(IFloodlightProviderService.class);
-        return l;
+// TODO Auto-generated method stub
+        Collection<Class<? extends IFloodlightService>> array = new ArrayList<Class<? extends
+                IFloodlightService>>();
+        array.add(IFloodlightProviderService.class);
+        array.add(IRestApiService.class);
+        return array;
     }
-
     @Override
     public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+// TODO Auto-generated method stub
         floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
         macAddresses = new ConcurrentSkipListSet<Long>();
         logger = LoggerFactory.getLogger(MACTracker.class);
+        restApi = context.getServiceImpl(IRestApiService.class);
     }
-
     @Override
-    public void startUp(FloodlightModuleContext context) {
+    public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+
+// TODO Auto-generated method stub
         floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+        restApi.addRestletRoutable(new MACTrackerWebRoutable());
     }
-
     @Override
-    public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-        Ethernet eth =
-                IFloodlightProviderService.bcStore.get(cntx,
-                        IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-
+    public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+// TODO Auto-generated method stub
+        Ethernet eth = IFloodlightProviderService.bcStore.get(cntx,
+                IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+//Para obtener la MAC
         Long sourceMACHash = eth.getSourceMACAddress().getLong();
         if (!macAddresses.contains(sourceMACHash)) {
+//Para añadir a la lista
             macAddresses.add(sourceMACHash);
             logger.info("MAC Address: {} seen on switch: {}",
                     eth.getSourceMACAddress().toString(),
@@ -96,5 +105,10 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
         }
         return Command.CONTINUE;
     }
-
+    @Override
+    public Set<Long> getMac() {
+// TODO Auto-generated method stub
+        return macAddresses;
+    }
 }
+
